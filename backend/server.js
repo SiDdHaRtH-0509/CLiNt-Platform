31c993401dc8a45dbc55069789b0089ac4c270bf
SiDdHaRtH-0509 <clsiddharth7075@gmail.com>
2026-05-28 17:30:04 +0530

    Fixed Live demo link

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    process.env.CLIENT_URL,
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
